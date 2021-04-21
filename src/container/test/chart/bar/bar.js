9c0ab28c7d1ce4a70f7d56096da8babd289d6a01
张奥 <zhangao@zhangaodeMacBook-Pro.local>
2021-04-21 17:58:49 +0800

    bar and line

--- src/container/test/chart/bar/bar.js
+++ src/container/test/chart/bar/bar.js
@@ -1,5 +1,81 @@
-import React from 'react';
-
+import React, { useEffect, useRef } from 'react';
+import * as echarts from 'echarts';
 export default function bar() {
-  return <div className="module">bar</div>;
+  const myEcharts = useRef(null);
+  useEffect(() => {
+    initEcharts();
+    return () => {
+      window.removeEventListener('resize', screenAdapter);
+    };
+  }, []);
+
+  const initEcharts = () => {
+    myEcharts.current = echarts.init(document.getElementById('bar'));
+    myEcharts.current.setOption({
+      title: {
+        text: '柱状图',
+        textStyle: {
+          color: 'green',
+          fontSize: 16
+        },
+        left: 'center'
+      },
+      tooltip: {
+        // show: false
+        backgroundColor: '#333',
+        borderColor: '#333',
+        formatter: data => {
+          console.log(data);
+          return `${data.name}的${data.seriesName}是${data.data}<br/>哈哈哈`;
+        }
+      },
+      backgroundColor: 'rgba(128, 128, 128, 0.1)',
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {
+        name: 'y轴',
+        nameRotate: '90',
+        nameLocation: 'center',
+        nameTextStyle: {
+          color: 'red',
+          fontSize: 16,
+          padding: [0, 0, 15, 20]
+        }
+      },
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          barWidth: 30,
+          data: [5, 20, 36, 10, 10, 20],
+          itemStyle: {
+            normal: {
+              barBorderRadius: [15, 15, 0, 0],
+              color: function (params) {
+                return ['red', 'orange', 'yellow', 'green', 'blue', 'purple'][params.dataIndex];
+              }
+            }
+          }
+        }
+      ]
+    });
+    window.addEventListener('resize', screenAdapter);
+  };
+
+  const screenAdapter = () => {
+    const bar = document.getElementById('bar');
+    const fontSize = (bar.offsetWidth / 100) * 3.6;
+    const adapterOption = {
+      title: {
+        textStyle: {
+          fontSize: fontSize
+        }
+      }
+    };
+    myEcharts.current.setOption(adapterOption);
+    myEcharts.current.resize();
+  };
+
+  return <div className="module" id="bar"></div>;
 }
--- src/container/test/chart/line/line.js
+++ src/container/test/chart/line/line.js
@@ -1,5 +1,72 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import * as echarts from 'echarts';
 export default function line() {
-  return <div className="module">line</div>;
+  useEffect(() => {
+    const myChart = echarts.init(document.getElementById('line'));
+    myChart.setOption({
+      title: {
+        text: '折线'
+      },
+      legend: {
+        data: ['销量一', '销量二']
+      },
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        name: 'x轴',
+        nameLocation: 'center',
+
+        nameTextStyle: {
+          color: 'green',
+          fontSize: 16,
+          padding: [10, 0, 0, 0]
+        }
+      },
+      yAxis: {
+        type: 'value'
+      },
+      tooltip: {
+        backgroundColor: '#333',
+        borderColor: '#333',
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross',
+          label: {
+            backgroundColor: '#6a7985'
+          },
+          crossStyle: {
+            color: 'red'
+          },
+          lineStyle: {
+            color: '#000'
+          }
+        }
+      },
+      series: [
+        {
+          name: '销量一',
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line',
+          smooth: true,
+          itemStyle: {
+            color: 'green'
+          }
+        },
+        {
+          name: '销量二',
+          data: [15, 330, 124, 318, 135, 47, 360],
+          type: 'line',
+          smooth: true,
+          itemStyle: {
+            color: 'red'
+          }
+        }
+      ]
+    });
+  }, []);
+  return (
+    <div className="module" id="line">
+      line
+    </div>
+  );
 }
